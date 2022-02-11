e9de49f87f947ffbca46b2afdf53b05b9ae1a2e9
jacques28 <devjacques28i@gmail.com>
2022-02-11 15:37:33 +0000

    add style to the code

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-  
-    </div>
+    <section className="testimonial">
+      <div className="testimonial-wrapper">
+        <img
+          className="testimonial-avatar"src="../github.png"alt="image missing"/>
+        <div className="testimonial-quote">
+          <p>
+           Hello, jack
+          </p>
+        </div>
+        <p className="testimonial-name">
+          <span>Back at coding...</span>
+        </p>
+      </div>
+    </section>
   );
-}
-
+}  
 export default App;
